4b02441de711acf5bd90ff44ad04c5eb0423a6ed
Omid Ashtari <omid@Omids-Mac-mini-2.local>
2019-10-13 18:52:30 -0700

    fixed small error in objects arrays files, finished functions assignment

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -148,7 +148,7 @@ console.log(populationTotal);
 
 
 
-Stretch: If you haven't already, convert your array method callbacks into arrow functions.
+// Stretch: If you haven't already, convert your array method callbacks into arrow functions.
 
-*/
+
 
